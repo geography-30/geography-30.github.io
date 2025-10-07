6b4e21d291e30f2ff7c3211414fb595c37728d0a
class-80x <cansuakcay12@gmail.com>
2025-10-07 16:40:37 +0300

    sdsd

--- settings.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('settingsBtn').onclick=function(){
-  const m=prompt('Choose open mode: modal / page / blank',localStorage.getItem('openMode')||'modal');
-  if(m) localStorage.setItem('openMode',m);
-};
